3c145f8feb6375ba7595ec1a21209120e24c03ae
Ninibeth Teixeira <ninibethreina@gmail.com>
2020-01-20 15:33:40 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,21 @@
+class Post 
+  attr_accessor :name, :author  
+  @@all= []
+  def initialize (name, author=nil)
+    @name= name 
+    @author= author 
+    @@all << self 
+  end 
+  
+  def author_name 
+    if self.author 
+      self.author.name 
+    else 
+      nil
+    end 
+  end 
+  
+  def self.all 
+    @@all
+  end 
+end 
